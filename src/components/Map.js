e1c64a211cac87c96d85817c34f0f78eff727be3
Aran Ahmad <Aran.Ahmad@geodata.no>
2022-10-10 15:41:51 +0200

    small changes to comments

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,5 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useContext, useEffect } from "react";
+import { AppContext } from "../state/context";
 
 import esriConfig from '@arcgis/core/config.js';
 import MapView from "@arcgis/core/views/MapView";
@@ -11,13 +12,14 @@ const MapComponent = () => {
   // Required: Set this property to insure assets resolve correctly.
   esriConfig.assetsPath = './assets';
   const mapDiv = useRef(null);
+  const context = useContext(AppContext)
 
   // Opprett kartet
   useEffect(() => {
     if (mapDiv.current) {
       // Det første vi trenger er et Map objekt med bakgrunnskart
       // Konstruktøren er allerede i koden, men vi må velge bakgrunnskartet
-      // En liste med valg inner vi i API dokumentasjonen:
+      // En liste med valg finner vi i API dokumentasjonen:
       // https://developers.arcgis.com/javascript/latest/api-reference/esri-Map.html#basemap
       const map = new Map({
         basemap: ''
@@ -27,15 +29,15 @@ const MapComponent = () => {
       // Dokumentasjonen for MapView finnes her:
       // https://developers.arcgis.com/javascript/latest/api-reference/esri-views-MapView.html
       // Dersom man ønsker å se Trondheimsområdet er koordinatene for det
-          // ymin: 63.40182257265643,
-          // xmin: 10.227928161621094,
-          // ymax: 63.453731595863324,
-          // xmax: 10.560264587402344
+      // ymin: 63.40182257265643,
+      // xmin: 10.227928161621094,
+      // ymax: 63.453731595863324,
+      // xmax: 10.560264587402344
       new MapView({
         // MapView trenger minimum feltene:
         // map
         // container
-        // men det er og lurt å legge til feltet extent
+        // extent(valgfritt, men lurt å ha med)
       }).when(() => {
         // Når kartet er initialisert kan vi hente data
         // Vi har lagd noen hjelpefunksjoner i utils/featureUtils
@@ -44,13 +46,13 @@ const MapComponent = () => {
           // Etter at vi har hentet data må vi legge dette til i kartet
           // Det er en annen funksjon i featureUtils
           // denne gjør OSM data om til et kartlag som kan legges til i kartet
-          
+
           // etter å ha lagd kartlaget må dette legges til i kartet
-          // Dette er beskrevet i API dokumentsjonen for Map
+          // Dette er beskrevet i API dokumentasjonen for Map
         });
       });
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return <div className="mapDiv" ref={mapDiv}></div>;
