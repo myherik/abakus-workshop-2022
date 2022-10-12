ee7125a537f0c39ee12b707ffe2a22c11938ae6a
Marie Falch Orre <marie.orre@geodata.no>
2022-10-12 13:37:47 +0200

    Changed data source

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,18 @@
 import React, { useRef, useEffect, useContext } from "react";
 
-import esriConfig from '@arcgis/core/config.js';
+import esriConfig from "@arcgis/core/config.js";
 import MapView from "@arcgis/core/views/MapView";
 import Map from "@arcgis/core/Map";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+
 
 import { AppContext } from "../state/context";
-import { createOSMFeatureLayer, queryOSMData } from "../utils/featureUtils";
 import "../App.css";
 
 const MapComponent = () => {
   const context = useContext(AppContext);
   // Required: Set this property to insure assets resolve correctly.
-  esriConfig.assetsPath = './assets';
+  esriConfig.assetsPath = "./assets";
   const mapDiv = useRef(null);
 
   // Opprett kartet
@@ -22,8 +23,13 @@ const MapComponent = () => {
       // En liste med valg inner vi i API dokumentasjonen:
       // https://developers.arcgis.com/javascript/latest/api-reference/esri-Map.html#basemap
       const map = new Map({
-        basemap: 'gray-vector'
+        basemap: "gray-vector",
       });
+
+      const featureLayer = new FeatureLayer({
+        url: "https://services-eu1.arcgis.com/zci5bUiJ8olAal7N/arcgis/rest/services/OSM_Tourism_EU/FeatureServer/0",
+      });
+      map.add(featureLayer);
 
       // For å kunne vise kartet må dette legges til i et MapView
       // Dokumentasjonen for MapView finnes her:
@@ -35,35 +41,18 @@ const MapComponent = () => {
           ymin: 63.40182257265643,
           xmin: 10.227928161621094,
           ymax: 63.453731595863324,
-          xmax: 10.560264587402344
-        }
+          xmax: 10.560264587402344,
+        },
       }).when((mapView) => {
-        // Når kartet er initialisert kan vi hente data
-        // Vi har lagd noen hjelpefunksjoner i utils/featureUtils
-        // Den første henter data fra Open Streetmap, men trenger en bbox
-        queryOSMData(
-          "63.40182257265643,10.227928161621094,63.453731595863324,10.560264587402344"
-        ).then((result) => {
-          // Etter at vi har hentet data må vi legge dette til i kartet
-          // Det er en annen funksjon i featureUtils
-          // Denne gjør OSM data om til et kartlag som kan legges til i kartet
-          const featureLayer = createOSMFeatureLayer(result, context);
-
-          // Etter å ha lagd kartlaget må dette legges til i kartet
-          // Dette er beskrevet i API dokumentsjonen for Map
-          map.add(featureLayer);
-        });
         // Esri har mange widgets som er enkle å legge til i kartet
         // En av disse er locate widgeten, som flytter kartet til din possisjon
         // Widgeten må først opprettes, så plasseres på kartet
         // Dokumentasjon for dette er på:
         // https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-Locate.html
         // En god idé er å sette zoom nivået med scale attributten, f. eks scale 5000
-
         // Vi har også lagd en egen widget som kan legges til i kartet
         // Denne ligger i components/RouteWidget, men den er ikke helt ferdig enda
         // Den må først legges til i App.js fila, men det er noen feil med den
-
         // Det første vi må gjøre er å sørge for at Widgeten har tilgang til MapViewet
         // Dette kan gjøres ved å legge den til i contexten vår
         // Resten av feilene må løses i RouteWidget fila
@@ -73,6 +62,6 @@ const MapComponent = () => {
   }, []);
 
   return <div className="mapDiv" ref={mapDiv}></div>;
-}
+};
 
 export default MapComponent;
